825874bfba98486014cf8baae8370572d9d83913
mkwkomorowski <mkw.komorowski@gmail.com>
2024-04-27 13:27:58 +0200

    Zadanie zaliczeniowe

--- kodilla-docker-course/OLD_VERSIONS/quotes-app/backend/src/main/java/com/kodilla/quotes/repository/QuoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.quotes.repository;
-
-import com.kodilla.quotes.model.Quote;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuoteRepository extends CrudRepository<Quote, Long> {
-}
